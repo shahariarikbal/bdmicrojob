ab0af4c80b56db2d52b1edd35dfe4045a521cc44
Muntasir-Pranto <fahimmuntasir43@gmail.com>
2023-03-05 18:04:17 +0600

    gitignore file is fixed

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -34,15 +34,15 @@ class ComposerAutoloaderInitd9bc6a1610c7eb9e301ec6656c8eeb3a
         $loader->register(true);
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInitd9bc6a1610c7eb9e301ec6656c8eeb3a::$files;
-        $requireFile = static function ($fileIdentifier, $file) {
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
             if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
                 $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
 
                 require $file;
             }
-        };
+        }, null, null);
         foreach ($filesToLoad as $fileIdentifier => $file) {
-            ($requireFile)($fileIdentifier, $file);
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => 'dev-development',
-        'version' => 'dev-development',
-        'reference' => '8340b190d8ecdbf85e5b525164a74738bb52e737',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9eae8168a407d5cf44c918184367203d11260b88',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -395,9 +395,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => 'dev-development',
-            'version' => 'dev-development',
-            'reference' => '8340b190d8ecdbf85e5b525164a74738bb52e737',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9eae8168a407d5cf44c918184367203d11260b88',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -739,8 +739,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.0|2.0',
-                1 => '1.0.0 || 2.0.0 || 3.0.0',
+                0 => '1.0.0 || 2.0.0 || 3.0.0',
+                1 => '1.0|2.0',
             ),
         ),
         'psr/simple-cache' => array(
